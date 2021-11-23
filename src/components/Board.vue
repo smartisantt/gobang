7fe5ee75cf4fd4c4a293566a6737b9a4e22eb09f
allen <ququ250@qq.com>
2021-11-24 00:42:00 +0800

    完成游戏

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,46 +1,223 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from "vue";
+import Chess from "./Chess.vue";
 let isGameOver = false; // 游戏是否结束
-let whichOne = 'white'; // 一开始是白色的棋子
-const chessArr = ref([]);
-const board = ref(null);
+let whichOne = "white"; // 一开始是白色的棋子
+const chessArrRef = ref([]);
+const boardRef = ref(null);
+
+function exist(chessPoint) {
+  let result = chessArrRef.value.find((item) => {
+    return item.x === chessPoint.x && item.y === chessPoint.y;
+  });
+  return result === undefined ? true : false;
+}
 
 function handleClick(e, row, col) {
-  console.log(row, col);
-  console.log(e.offsetX, e.offsetY);
-
-  let tdw = (board.value.clientWidth * 0.93) / 14;
+  if (isGameOver) {
+    if (window.confirm("是否要重新开始一局？")) {
+      chessArrRef.value = []; // 重置棋子的数组
+      isGameOver = false;
+    }
+    return;
+  }
+  let tdw = (boardRef.value.clientWidth * 0.92) / 14;
 
   let positionX = e.offsetX > tdw / 2;
   let positionY = e.offsetY > tdw / 2;
 
   let chessPoint = {
-    x: positionX ? parseInt(col) : parseInt(col) - 1,
-    y: positionY ? parseInt(row) : parseInt(row) - 1,
+    x: positionX ? parseInt(col) + 1 : parseInt(col),
+    y: positionY ? parseInt(row) + 1 : parseInt(row),
     c: whichOne,
   };
 
-  chessArr.push(chessPoint);
+  if (exist(chessPoint) && !isGameOver) {
+    chessArrRef.value.push(chessPoint);
+    check();
+    whichOne = whichOne === "white" ? "black" : "white"; // 切换棋子的颜色
+  }
+}
 
-  whichOne = whichOne === 'white' ? 'black' : 'white'; // 切换棋子的颜色
+// 检查游戏是否结束，检查是否有符合要求的棋子
+function check() {
+  // 其实就是遍历数组里面的每一个棋子
+  // 这里分为 4 种情况：横着、竖着、斜着（2 种）
+
+  for (let i = 0; i < chessArrRef.value.length; i++) {
+    let curChess = chessArrRef.value[i];
+    let chess2, chess3, chess4, chess5;
+
+    // 检查有没有横着的 5 个颜色一样的棋子
+    chess2 = chessArrRef.value.find(function (item) {
+      return (
+        curChess.x === item.x + 1 &&
+        curChess.y === item.y &&
+        curChess.c === item.c
+      );
+    });
+    chess3 = chessArrRef.value.find(function (item) {
+      return (
+        curChess.x === item.x + 2 &&
+        curChess.y === item.y &&
+        curChess.c === item.c
+      );
+    });
+    chess4 = chessArrRef.value.find(function (item) {
+      return (
+        curChess.x === item.x + 3 &&
+        curChess.y === item.y &&
+        curChess.c === item.c
+      );
+    });
+    chess5 = chessArrRef.value.find(function (item) {
+      return (
+        curChess.x === item.x + 4 &&
+        curChess.y === item.y &&
+        curChess.c === item.c
+      );
+    });
+    if (chess2 && chess3 && chess4 && chess5) {
+      // 进入此 if，说明游戏结束
+      end(curChess, chess2, chess3, chess4, chess5);
+    }
+
+    // 检查有没有竖着的 5 个颜色一样的棋子
+    chess2 = chessArrRef.value.find(function (item) {
+      return (
+        curChess.x === item.x &&
+        curChess.y === item.y + 1 &&
+        curChess.c === item.c
+      );
+    });
+    chess3 = chessArrRef.value.find(function (item) {
+      return (
+        curChess.x === item.x &&
+        curChess.y === item.y + 2 &&
+        curChess.c === item.c
+      );
+    });
+    chess4 = chessArrRef.value.find(function (item) {
+      return (
+        curChess.x === item.x &&
+        curChess.y === item.y + 3 &&
+        curChess.c === item.c
+      );
+    });
+    chess5 = chessArrRef.value.find(function (item) {
+      return (
+        curChess.x === item.x &&
+        curChess.y === item.y + 4 &&
+        curChess.c === item.c
+      );
+    });
+    if (chess2 && chess3 && chess4 && chess5) {
+      // 进入此 if，说明游戏结束
+      end(curChess, chess2, chess3, chess4, chess5);
+    }
+
+    // 检查有没有斜着的 5 个颜色一样的棋子
+    chess2 = chessArrRef.value.find(function (item) {
+      return (
+        curChess.x === item.x + 1 &&
+        curChess.y === item.y + 1 &&
+        curChess.c === item.c
+      );
+    });
+    chess3 = chessArrRef.value.find(function (item) {
+      return (
+        curChess.x === item.x + 2 &&
+        curChess.y === item.y + 2 &&
+        curChess.c === item.c
+      );
+    });
+    chess4 = chessArrRef.value.find(function (item) {
+      return (
+        curChess.x === item.x + 3 &&
+        curChess.y === item.y + 3 &&
+        curChess.c === item.c
+      );
+    });
+    chess5 = chessArrRef.value.find(function (item) {
+      return (
+        curChess.x === item.x + 4 &&
+        curChess.y === item.y + 4 &&
+        curChess.c === item.c
+      );
+    });
+    if (chess2 && chess3 && chess4 && chess5) {
+      // 进入此 if，说明游戏结束
+      end(curChess, chess2, chess3, chess4, chess5);
+    }
+
+    // 检查有没有斜着的 5 个颜色一样的棋子
+    chess2 = chessArrRef.value.find(function (item) {
+      return (
+        curChess.x === item.x - 1 &&
+        curChess.y === item.y + 1 &&
+        curChess.c === item.c
+      );
+    });
+    chess3 = chessArrRef.value.find(function (item) {
+      return (
+        curChess.x === item.x - 2 &&
+        curChess.y === item.y + 2 &&
+        curChess.c === item.c
+      );
+    });
+    chess4 = chessArrRef.value.find(function (item) {
+      return (
+        curChess.x === item.x - 3 &&
+        curChess.y === item.y + 3 &&
+        curChess.c === item.c
+      );
+    });
+    chess5 = chessArrRef.value.find(function (item) {
+      return (
+        curChess.x === item.x - 4 &&
+        curChess.y === item.y + 4 &&
+        curChess.c === item.c
+      );
+    });
+    if (chess2 && chess3 && chess4 && chess5) {
+      end(curChess, chess2, chess3, chess4, chess5);
+    }
+  }
+}
+
+function end() {
+  if (!isGameOver) {
+    isGameOver = true;
+
+    // 2. 把获胜的棋子加上一个红色阴影
+    for (let i = 0; i < arguments.length; i++) {
+      arguments[i].win = "win";
+    }
+  }
 }
 </script>
 
 <template lang="">
-    <div class="container">
-        <table class="chessboard" ref="board">
-            <tr v-for="iRow in 14" :key="iRow">
-                <td v-for="iCol in 14" :key="iCol" @click="handleClick($event, iRow, iCol)">
-                  <div class="chess black"></div>
-                </td>
-            
-            </tr>
-        </table>
-    </div>
+  <div class="container">
+    <table class="chessboard" ref="boardRef">
+      <tr v-for="y in 14" :key="y">
+        <td
+          v-for="x in 14"
+          :key="x"
+          @click="handleClick($event, y, x)"
+        >
+          
+          <Chess v-if="x===14" :point="{x:15,y}" :chessArr="chessArrRef" :style="{left:'50%'}" />
+          <Chess v-if="y===14" :point="{x,y:15}" :chessArr="chessArrRef" :style="{top:'50%'}" />
+          <Chess v-if="x===14 && y===14" :point="{x:15,y:15}" :chessArr="chessArrRef" :style="{left:'50%',top:'50%'}" />
+          <Chess :point="{x,y}" :chessArr="chessArrRef" />
+        </td>
+      </tr>
+    </table>
+  </div>
 </template>
 
-
-<style >
+<style>
 .container {
   border: 1px solid rgb(207, 188, 188);
   width: 500px;
@@ -64,39 +241,5 @@ function handleClick(e, row, col) {
 .chessboard td {
   border: 1px solid black;
   position: relative;
-}
-
-/* 棋子的公共样式 */
-.chess {
-  border: 1px solid lightgrey;
-  border-radius: 50%;
-  position: absolute;
-  left: -50%;
-  top: -50%;
-  width: 90%;
-  height: 90%;
-  color: lightgrey;
-  font-size: 12px;
-  font-weight: bold;
-  /* 让文字居中 */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-/* 白色棋子 */
-.white {
-  background-color: #fff;
-}
-
-/* 黑色棋子 */
-.black {
-  background-color: #000;
-}
-
-/* 获胜棋子 */
-.win {
-  border: 1px solid red;
-  box-shadow: 0 0 3px 2px red; /* 红色阴影 */
 }
 </style>
---
+++ src/components/Chess.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { computed } from "vue";
+const props = defineProps({
+  point: Object,
+  chessArr: Array,
+});
+
+const chess = computed(() =>
+  props.chessArr.find((it) => {
+    return it.x == props.point.x && it.y == props.point.y;
+  })
+);
+</script>
+
+<template>
+  <div
+    v-if="chess"
+    :class="{
+      chess: Boolean(chess),
+      white: chess.c === 'white',
+      black: chess.c === 'black',
+      win: chess.win === 'win',
+    }"
+    @click.stop
+  ></div>
+</template>
+
+ 
+
+<style>
+/* 棋子的公共样式 */
+.chess {
+  border: 1px solid lightgrey;
+  border-radius: 50%;
+  position: absolute;
+  left: -50%;
+  top: -50%;
+  width: 90%;
+  height: 90%;
+  color: lightgrey;
+  font-size: 12px;
+  font-weight: bold;
+  /* 让文字居中 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+/* 白色棋子 */
+.white {
+  background-color: #fff;
+}
+
+/* 黑色棋子 */
+.black {
+  background-color: #000;
+}
+
+/* 获胜棋子 */
+.win {
+  border: 1px solid red;
+  box-shadow: 0 0 3px 2px red; /* 红色阴影 */
+}
+</style>
